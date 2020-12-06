062eebb5972c8688ccd2577f272dac44ee841938
malinghan <linghan.ma@gmail.com>
2020-12-06 23:11:44 +0800

    add

---
+++ Week_06/init.sql
@@ -0,0 +1,6 @@
+
+
+
+insert info t_order(order_id,user_id,address_id ,status)
+values
+("")
---
+++ Week_06/query.sql
@@ -0,0 +1,12 @@
+-- 数据查询
+DROP PROCEDURE IF EXISTS order_query;
+DELIMITER $
+CREATE PROCEDURE order_query()
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    WHILE i<=1000000 DO
+        select * from demo_ds.t_order where id = i;
+        SET i = i+1;
+    END WHILE;
+END $
+CALL order_query();
