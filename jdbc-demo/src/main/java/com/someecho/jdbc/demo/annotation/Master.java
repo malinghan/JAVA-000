5792b5865b944b62a4b1e04ca0d0c26a2eb25d53
malinghan <linghan.ma@gmail.com>
2020-12-10 23:59:35 +0800

    add

---
+++ jdbc-demo/src/main/java/com/someecho/jdbc/demo/annotation/Master.java
@@ -0,0 +1,10 @@
+package com.someecho.jdbc.demo.annotation;
+
+/**
+ * @author : linghan.ma
+ * @Package com.someecho.jdbc.demo.annotation
+ * @Description:
+ * @date Date : 2020年12月10日 11:57 PM
+ **/
+public @interface Master {
+}
---
+++ jdbc-demo/src/main/java/com/someecho/jdbc/demo/enums/DBTypeEnum.java
@@ -0,0 +1,13 @@
+package com.someecho.jdbc.demo.enums;
+
+/**
+ * @author : linghan.ma
+ * @Package com.someecho.jdbc.demo.enums
+ * @Description:
+ * @date Date : 2020年12月10日 11:49 PM
+ **/
+public enum  DBTypeEnum {
+    MASTER,
+    SLAVE1,
+    SLAVE2
+}
